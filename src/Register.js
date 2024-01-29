89afba39e2946d532f8b66247a06adcb04d3a939
amitsharma7565 <amitsharma7565@gmail.com>
2024-01-29 18:26:30 +0530

    Register page

--- src/Register.js
+++ src/Register.js
@@ -1,11 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Register(){
+
     return(
         <form>      
         <div>
            <h1>Register</h1>
            <p>Please fill the below details to create an account</p>
            <hr/>
-           <label for="email" >Email</label>
+           <label for="email">Email</label>
            <input type="text" placeholder="Enter Email" name="email" id="email"/><br/> <br/> 
            <label for ="psw">Password</label> 
            <input type="password" placeholder="Enter password" name="psw" id="psw"/><br/> <br/> 

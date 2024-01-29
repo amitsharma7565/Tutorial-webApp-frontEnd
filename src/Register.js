36f86f6ff80256322a7b43d27e3e809d5bab4232
amitsharma7565 <amitsharma7565@gmail.com>
2024-01-29 12:11:50 +0530

    Register page

--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,20 @@
 function Register(){
     return(
+        <form>      
         <div>
-            <h1>Welcome to Register page</h1>
+           <h1>Register</h1>
+           <p>Please fill the below details to create an account</p>
+           <hr/>
+           <label for="email" >Email</label>
+           <input type="text" placeholder="Enter Email" name="email" id="email"/><br/> <br/> 
+           <label for ="psw">Password</label> 
+           <input type="password" placeholder="Enter password" name="psw" id="psw"/><br/> <br/> 
+           <label for ="rPsw" >Repeat Password</label>
+           <input type="password" placeholder="Repeat password" name="rPsw" id="rPsw"/> <br/><br/>
+           <p>By creating an account you agree to our <a href="https://www.google.com">Terms & Privacy</a>. </p>
+            <button type="submit" class="registerBtn" >Register</button>
         </div>
+        </form>
     )
 }
 

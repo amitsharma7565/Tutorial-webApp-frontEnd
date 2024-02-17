3874123b23ba85b09145a2f1fe50ae5d87141028
amitsharma7565 <amitsharma7565@gmail.com>
2024-02-17 16:55:19 +0530

    Dashboard done

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,15 +1,43 @@
+
 import './dashboard.css'
+
 function Dashboard(){
     return(
-
         <div class ='edit' >
             <h1> Welcome to dashboard page</h1>
             <p>
                 Take a online courses from the world best online eduction-HUB 
             </p>
-            
-        </div>
+            <div class= 'container'>
+            <div class= 'box'>
+              <p >School Stundent<a href=''>Click here </a></p>  
+            </div>
+            <div class= 'box'>
+              <p>College Student <a href=''>Click Here</a></p>
+            </div>
+            <div class= 'box'>
+                <p>Working professional<a href=''>Click Here</a></p>
+            </div>
+            </div>
+            <div class="footerPart">
+            <p>Top companies choose this online eduction hub to build in-demand career skills.</p>
+            <text>
+            © 2024 Eduction, Inc.
+            </text>
+            <th>
+                <tr>
+                    Term & Condition     
+                </tr>
+                <tr>
+                    Get a app     
+                </tr>
+                <tr>
+                    Contact us   
+                </tr>
+            </th>
+            </div>
 
+        </div>
     )
 }
 

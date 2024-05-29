63b2132126acaf57c6662e57eaa9a6e47c759ac0
amitsharma7565 <amitsharma7565@gmail.com>
2024-05-29 22:25:30 +0530

    Routing in welcome page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="App">
        <Routes>
+        <Route path="/" element={<Login/>}/>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/RegisterDone" element={<RegisterDone/>}/>
